c703626a710916ab77c0cf0f37992b7779d35b89
Florian Katerndahl <florian@katerndahl.com>
2023-10-19 19:00:57 +0000

    Thu, Oct 19, 2023, 7:00 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,14 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string name;
+    do
+    {
+        name = get_string("What's your name? ");
+    }
+    while (name != NULL);
+
+    printf("hello, %s\n", name);
 }
