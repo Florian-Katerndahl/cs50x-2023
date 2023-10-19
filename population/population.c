f88b1daf1d2f22bab920e339f81190e6b7016810
Florian Katerndahl <florian@katerndahl.com>
2023-10-19 18:57:20 +0000

    Thu, Oct 19, 2023, 6:57 PM +00:00

--- population/population.c
+++ population/population.c
@@ -8,23 +8,26 @@ int main(void)
     do
     {
         start_size = get_long("Enter the start size of your llama population: ");
-    } while (start_size < 9);
+    }
+    while (start_size < 9);
 
     /* Prompt for end size
-    *  Dislike the style guide, this isn't neater, but whatever
-    *   while ((end_size = get_long("Enter the final size of your llama population: ")) < start_size);
-    *  HOWEVER: Usually, you shouldn't be smart about in **this** regard
-    */
+     *  Dislike the style guide, this isn't neater, but whatever
+     *   while ((end_size = get_long("Enter the final size of your llama population: ")) < start_size);
+     *  HOWEVER: Usually, you shouldn't be smart about in **this** regard
+     */
     long end_size = 0;
     do
     {
         end_size = get_long("Enter the final size of your llama population: ");
-    } while (end_size < start_size);
+    }
+    while (end_size < start_size);
 
     // Calculate number of years until we reach threshold
     int year = 0;
     long curr = start_size;
-    while (curr < end_size) {
+    while (curr < end_size)
+    {
         curr += curr / 3 - curr / 4;
         year++;
     }
