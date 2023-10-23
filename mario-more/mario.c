76d8193f7ebfca703687294e12eca9cf82be5a3a
Florian Katerndahl <florian@katerndahl.com>
2023-10-23 19:25:05 +0000

    Mon, Oct 23, 2023, 7:25 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,18 +7,18 @@ void print_hashes(int row);
 
 int main(void)
 {
-
-
-    for (int i = 0; i < get_height; i++)
+    int height = get_height();
+    for (int i = 0; i < height; i++)
     {
         print_spaces(height, i);
         print_hashes(i);
         printf(" ");
-        print_hashes(i)
+        print_hashes(i);
 
         printf("\n");
     }
 
+    return 0;
 }
 
 int get_height(void)
@@ -44,7 +44,7 @@ void print_spaces(int total_height, int row)
 void print_hashes(int row)
 {
     for (int j = 0; j < row + 1; j++)
-        {
-            printf("#");
-        }
+    {
+        printf("#");
+    }
 }
