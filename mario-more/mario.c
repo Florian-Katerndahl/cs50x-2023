fbd6b8b5c90c48fd99019dc30fad514ad5b065fc
Florian Katerndahl <florian@katerndahl.com>
2023-10-23 19:21:08 +0000

    Mon, Oct 23, 2023, 7:21 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_left_stairs(int size);
-void print_right_stairs(int size);
+void print_spaces(int size);
+void print_hashes(int size);
 
 int main(void)
 {
@@ -13,4 +13,26 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+}
+
+void print_spaces(int total_height, int row)
+{
+    for (int j = 0; j < total_height - row - 1; j++)
+    {
+        printf(" ");
+    }
 }
