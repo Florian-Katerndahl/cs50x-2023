edabf5f387830fd545dc628a64f857bc2688d418
Florian Katerndahl <florian@katerndahl.com>
2023-10-23 19:23:10 +0000

    Mon, Oct 23, 2023, 7:23 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,10 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_spaces(int size);
-void print_hashes(int size);
+int get_height(void);
+void print_spaces(int total_height, int row);
+void print_hashes(int row);
 
 int main(void)
+{
+
+
+    for (int i = 0; i < get_height; i++)
+    {
+        print_spaces(height, i);
+        print_hashes(i);
+        printf(" ");
+        print_hashes(i)
+
+        printf("\n");
+    }
+
+}
+
+int get_height(void)
 {
     int height;
     do
@@ -13,20 +30,7 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < height - i - 1; j++)
-        {
-            printf(" ");
-        }
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-
-        printf("\n");
-    }
-
+    return height;
 }
 
 void print_spaces(int total_height, int row)
@@ -35,4 +39,12 @@ void print_spaces(int total_height, int row)
     {
         printf(" ");
     }
+}
+
+void print_hashes(int row)
+{
+    for (int j = 0; j < row + 1; j++)
+        {
+            printf("#");
+        }
 }
