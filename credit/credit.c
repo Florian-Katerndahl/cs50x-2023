797824354f973d05ed91e7abcd4751830d6370f6
Florian Katerndahl <florian@katerndahl.com>
2023-10-26 20:06:02 +0000

    Thu, Oct 26, 2023, 8:06 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void to_string(long cn, char dest[256]);
 int is_valid(long cn);
 int is_amex(long cn);
 int is_master(long cn);
@@ -8,6 +9,7 @@ int is_visa(long cn);
 
 int main(void)
 {
+    char num_as_string[256] = { 0 };
     long card_number;
     do
     {
@@ -15,7 +17,8 @@ int main(void)
     }
     while (card_number < 0);
 
-    printf("%zu", sizeof(long));
+    to_string(card_number, num_as_string);
+    printf("%s\n", num_as_string);
     if (!is_valid(card_number)) printf("INVALID\n");
     else if (is_amex(card_number)) printf("AMEX\n");
     else if (is_master(card_number)) printf("MASTERCARD\n");
@@ -25,15 +28,14 @@ int main(void)
     return 0;
 }
 
-char[256] to_string(long cn)
+void to_string(long cn, char dest[256])
 {
-    char num_as_string[256];
     for (int i = 0; cn > 0; i++)
     {
-        num_as_string[i] = cn % 10;
+        printf(")
+        dest[i] = cn % 10;
         cn /= 10;
     }
-    return num_as_string;
 }
 
 int is_valid(long cn)
