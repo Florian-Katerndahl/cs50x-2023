6427d79beab6776d895b07752bc0030263e34ea9
Florian Katerndahl <florian@katerndahl.com>
2023-10-26 21:48:33 +0000

    Thu, Oct 26, 2023, 9:48 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,7 +9,7 @@ int is_visa(int src[256], int digits);
 
 int main(void)
 {
-    int card_arr[256] = { 0 };
+    int card_arr[256] = {0};
     long card_number;
     do
     {
@@ -19,11 +19,16 @@ int main(void)
 
     int nDigits = to_digits(card_number, card_arr);
 
-    if (!is_valid(card_arr, nDigits)) printf("INVALID\n");
-    else if (is_amex(card_arr, nDigits)) printf("AMEX\n");
-    else if (is_master(card_arr, nDigits)) printf("MASTERCARD\n");
-    else if (is_visa(card_arr, nDigits)) printf("VISA\n");
-    else printf("INVALID\n");
+    if (!is_valid(card_arr, nDigits))
+        printf("INVALID\n");
+    else if (is_amex(card_arr, nDigits))
+        printf("AMEX\n");
+    else if (is_master(card_arr, nDigits))
+        printf("MASTERCARD\n");
+    else if (is_visa(card_arr, nDigits))
+        printf("VISA\n");
+    else
+        printf("INVALID\n");
 
     return 0;
 }
@@ -45,7 +50,7 @@ int is_valid(int src[256], int digits)
     int chksm = 0;
     for (int i = 0; i < digits; i++)
     {
-        int tmp = src[i] * ( 1 + (i % 2));
+        int tmp = src[i] * (1 + (i % 2));
         chksm += tmp % 10 + ((tmp / 10) % 10);
     }
     return !(chksm % 10);
@@ -53,15 +58,15 @@ int is_valid(int src[256], int digits)
 
 int is_amex(int src[256], int digits)
 {
-    return digits == 15 && src[digits-1] == 3 && (src[digits-2] == 5 || src[digits-2] == 7);
+    return digits == 15 && src[digits - 1] == 3 && (src[digits - 2] == 5 || src[digits - 2] == 7);
 }
 
 int is_master(int src[256], int digits)
 {
-    return digits == 16 && src[digits-1] == 5 && src[digits-2] > 0 &&  src[digits-2] < 6;
+    return digits == 16 && src[digits - 1] == 5 && src[digits - 2] > 0 && src[digits - 2] < 6;
 }
 
 int is_visa(int src[256], int digits)
 {
-    return (digits == 13 || digits == 16) && src[digits-1] == 4;
+    return (digits == 13 || digits == 16) && src[digits - 1] == 4;
 }
