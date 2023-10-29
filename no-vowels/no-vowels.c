746b6883bf0b80874a312fb783e125110a40f7b1
Florian Katerndahl <florian@katerndahl.com>
2023-10-29 16:12:27 +0000

    Sun, Oct 29, 2023, 4:12 PM +00:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 
-string replace(string input);
+string replace(string input[]);
 
 int main(int argc, string argv[])
 {
@@ -17,14 +17,14 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    string converted = replace(*(argv + 1));
+    string converted = replace(&argv[1]);
     printf("%s\n", converted);
     return 0;
 }
 
-string replace(string input)
+string replace(string input[])
 {
-    int length = strlen(input) + 1;
+    int length = strlen(*input) + 1;
     string converted[length] = {0};
     for (int i = 0, l = strlen(input); i < l; i++)
     {
