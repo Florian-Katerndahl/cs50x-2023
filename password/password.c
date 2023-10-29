9190f58eb7f8131d67a7021044304acc447244d8
Florian Katerndahl <florian@katerndahl.com>
2023-10-29 16:28:24 +0000

    Sun, Oct 29, 2023, 4:28 PM +00:00

--- password/password.c
+++ password/password.c
@@ -23,6 +23,22 @@ int main(void)
 // TODO: Complete the Boolean function below
 bool valid(string password)
 {
-    bool 
-    return false;
+    bool contains_lower = false;
+    bool contains_upper = false;
+    bool contains_number = false;
+    bool contains_symbol = false;
+    char *p = password;
+    while (*p)
+    {
+        if (*p > 64 && *p < 91)
+            contains_upper = true;
+        if (*p > 96 && *p < 123)
+            contains_lower = true;
+        if (*p > 47 && *p < 58)
+            contains_number = true;
+        if (*p > 32 && *p < 48)
+            contains_symbol = true;
+        p++;
+    }
+    return contains_lower && contains_upper && contains_number && contains_symbol;
 }
