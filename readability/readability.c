893756c980dc821f9513ffa16179d8894563158c
Florian Katerndahl <florian@katerndahl.com>
2023-10-30 19:12:40 +0000

    Mon, Oct 30, 2023, 7:12 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+float coleman_liau(const char * text);
+
 int main(void)
 {
+    char * text;
+    do
+    {
+        text = get_string("Text: ");
+    }
+    while (text == NULL);
 
+    float grade = coleman_liau(text);
+
+
+
+    return 0;
+}
+
+float coleman_liau(const char * text)
+{
+    return 0.0588 * ((float) L) - 0.296 * ((float) S) - 15.8;
 }
