875979dec02fa1b8f10dba83f82d2a3d39efa7c9
Florian Katerndahl <florian@katerndahl.com>
2023-10-30 19:23:28 +0000

    Mon, Oct 30, 2023, 7:23 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,4 +1,7 @@
+#include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 float coleman_liau(const char * text);
 
@@ -25,8 +28,21 @@ int main(void)
 
 float coleman_liau(const char * text)
 {
-    int word_count = 0;
-    int sentence_count = 0;
-    
-    return 0.0588 * ((float) L) - 0.296 * ((float) S) - 15.8;
+    unsigned int letter_count = 0;
+    unsigned int word_count = 0;
+    unsigned int sentence_count = 0;
+    size_t ssize = strlen(text);
+
+    for (size_t i = 0; i < ssize; i++)
+    {
+        char c = text[i];
+        if ((c > 64 && c < 91) || (c > 96 && c < 123))
+            letter_count++;
+        if (c == ' ')
+            word_count++;
+        if (c == '.' || c == ',' || c == '?' || c == '!' || c == ';' || c == ':')
+            sentence_count++;
+    }
+
+    return 0.0588 * (((float) letter_count) / ((float) word_count) * 100.0) - 0.296 *  (((float) sentence_count) / ((float) word_count) * 100.0) - 15.8;
 }
