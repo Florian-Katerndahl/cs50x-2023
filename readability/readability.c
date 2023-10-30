9f9ded394e229c0f736bfc281e74ec2e76c2ef07
Florian Katerndahl <florian@katerndahl.com>
2023-10-30 19:14:49 +0000

    Mon, Oct 30, 2023, 7:14 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,14 +11,22 @@ int main(void)
     }
     while (text == NULL);
 
-    float grade = coleman_liau(text);
+    int grade = (int) roundf(coleman_liau(text));
 
-
+    if (grade > 16)
+        printf("Grade 16+\n");
+    else if (grade < 1)
+        printf("Before grade 1\n");
+    else
+        printf("Grade %d\n", grade);
 
     return 0;
 }
 
 float coleman_liau(const char * text)
 {
+    int word_count = 0;
+    int sentence_count = 0;
+    
     return 0.0588 * ((float) L) - 0.296 * ((float) S) - 15.8;
 }
