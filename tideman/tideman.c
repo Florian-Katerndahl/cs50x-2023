97c8936a8f02a80971094b40362f158fda5eb56e
Florian Katerndahl <florian@katerndahl.com>
2024-01-10 14:17:09 +0000

    Wed, Jan 10, 2024, 2:17 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -127,9 +127,11 @@ void add_pairs(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i + 1; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
-            preferences[rank[i]][rank[j]]++;
+            if (i == j) continue;
+            if (preferences[i][j])
+            pair[pair_count]
         }
     }
     return;
