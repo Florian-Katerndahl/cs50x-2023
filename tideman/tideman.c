619b930472871088d3b55c4a6d6bbf1490bb741e
Florian Katerndahl <florian@katerndahl.com>
2024-01-10 15:01:44 +0000

    Wed, Jan 10, 2024, 3:01 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -93,6 +93,14 @@ int main(int argc, string argv[])
     sort_pairs();
     lock_pairs();
     print_winner();
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            printf("%d ", locked[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
 
