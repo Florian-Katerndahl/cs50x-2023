dfd00abeaddff15befbebdca8ee91f47972b3617
Florian Katerndahl <florian@katerndahl.com>
2024-01-12 14:50:03 +0000

    Fri, Jan 12, 2024, 2:50 PM +00:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
         return 4;
     }
 
-    bi.
+    bi.biHeight *= -1;
 
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
