7044b053517606c34808316b20cd90bfb05e83d8
Florian Katerndahl <florian@katerndahl.com>
2024-01-15 20:39:42 +0000

    Mon, Jan 15, 2024, 8:39 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -65,7 +65,8 @@ int main(int argc, char *argv[])
     uint16_t data;
     while (fread(&data, sizeof(data), 1, input))
     {
-        data = (uint16_t) round((float) (data) * factor); // result was truncated and not rounded....#
+        //data = (uint16_t) round((float) (data) * factor); // result was truncated and not rounded....#
+        data *= factor;
         fwrite(&data, sizeof(data), 1, output);
     }
 
