85b62c506b0a3dc28f55f296c8bafb8e89644a5f
Florian Katerndahl <florian@katerndahl.com>
2024-01-15 19:31:56 +0000

    Mon, Jan 15, 2024, 7:31 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,17 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    for (int x = 0; x < height; x++)
+    {
+        for (y = 0; y < width; y++)
+        {
+            if (image[x][y].rgbtBlue == 0x00 && image[x][y].rgbtGreen == 0x00 && image[x][y].rgbtRed == 0x00)
+            {
+                 image[x][y].rgbtBlue = 0x00;
+                 image[x][y].rgbtGreen = 0x00;
+                 image[x][y].rgbtRed = 0x00;
+            }
+        }
+    }
+    return;
 }
