ab02b6d321123960dbd47877f40ea46d7bebb016
Florian Katerndahl <florian@katerndahl.com>
2024-01-15 19:33:53 +0000

    Mon, Jan 15, 2024, 7:33 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -4,13 +4,13 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int x = 0; x < height; x++)
     {
-        for (y = 0; y < width; y++)
+        for (int y = 0; y < width; y++)
         {
             if (image[x][y].rgbtBlue == 0x00 && image[x][y].rgbtGreen == 0x00 && image[x][y].rgbtRed == 0x00)
             {
-                 image[x][y].rgbtBlue = 0x00;
-                 image[x][y].rgbtGreen = 0x00;
-                 image[x][y].rgbtRed = 0x00;
+                 image[x][y].rgbtBlue = 0xf0;
+                 image[x][y].rgbtGreen = 0xf0;
+                 image[x][y].rgbtRed = 0x3a;
             }
         }
     }
