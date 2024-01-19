0fcc1bef89a73884409bd7159500ca1f7a144e12
Florian Katerndahl <florian@katerndahl.com>
2024-01-19 14:39:07 +0000

    Fri, Jan 19, 2024, 2:39 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,8 +1,23 @@
 #include "helpers.h"
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            uint8_t avg = (uint8_t) round((image[y][x].rgbtBlue + image[y][x].rgbtGreen + image[y][x].rgbtRed) / 3.0);
+
+            image[y][x].rgbtBlue = avg;
+            image[y][x].rgbtGreen = avg;
+            image[y][x].rgbtRed = avg;
+        }
+    }
     return;
 }
 
