7d9df7d0787a5b12e84b3b4e4ff4edfcaf326443
Florian Katerndahl <florian@katerndahl.com>
2024-01-19 15:05:15 +0000

    Fri, Jan 19, 2024, 3:05 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -81,11 +81,11 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE(*copy)[width] = calloc(height, width * sizeof(RGBTRIPLE));
 
-    int Gx_kernel[3][3] = {{-1, 0, 1,},
+    static int Gx_kernel[3][3] = {{-1, 0, 1,},
                            {-2, 0, 2,},
                            {-1, 0, 1}};
 
-    int Gy_kernel[3][3] = {{-2, -2, -1,},
+    static int Gy_kernel[3][3] = {{-2, -2, -1,},
                            {0,  0,  0,},
                            {1,  2,  1}};
 
