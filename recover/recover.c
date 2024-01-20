b30833f34e5263005ed023895a9d8458e23f5a9d
Florian Katerndahl <florian@katerndahl.com>
2024-01-20 12:15:09 +0000

    Sat, Jan 20, 2024, 12:15 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 uint8_t JPEG_signature[4] = {0xff, 0xd8, 0xff, 0xe0}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -14,7 +15,17 @@ int main(int argc, char *argv[])
     FILE *infile = fopen(argv[1], "rb");
     if (infile == NULL)
     {
-        fprintf(stderr, "Error: Coul not read file %s\n", argv[1)
+        fprintf(stderr, "Error: Coul not read file %s\n", argv[1]);
+        return 1;
+    }
+
+    char outname[8] = { 0 };
+    uint8_t data[512];
+    size_t out_index = 0;
+
+    while (fread(data, 1, 512, infile) == 512)
+    {
+        if (data[0] == 0xff && data[1] == 0xd8 && data[2] == 0xff && ((data[3] & 0xe0) == 0xe0))
     }
 
     return 0;
