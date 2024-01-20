35502bed95a5aa33dad3f1da58c8cf5839e2e5bd
Florian Katerndahl <florian@katerndahl.com>
2024-01-20 12:11:01 +0000

    Sat, Jan 20, 2024, 12:11 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdint.h>
+
+uint8_t JPEG_signature[4] = {0xff, 0xd8, 0xff, 0xe0}
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    FILE *infile = fopen(argv[1], "rb");
+    if (infile == NULL)
+    {
+        fprintf(stderr, "Error: Coul not read file %s\n", argv[1)
+    }
+
+    return 0;
 }
