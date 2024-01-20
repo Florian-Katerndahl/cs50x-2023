1935f6f1caf4d3b61f23662ae2f70539ce43b29b
Florian Katerndahl <florian@katerndahl.com>
2024-01-20 18:32:09 +0000

    Sat, Jan 20, 2024, 6:32 PM +00:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -117,7 +117,7 @@ int main(int argc, char *argv[])
         }
         for (int j = 0; j < block_size; j++)
         {
-            data[i *block_size+ j] = data[header.subchunk2Size - 1 - ((1 + i) * block_size - 1) + j];
+            data[i *block_size+ j] = data[header.subchunk2Size - ((1 + i) * block_size) + j];
             printf("%d, %d, %d\n", i, header.subchunk2Size - ((1 + i) * block_size) + j, header.subchunk2Size);
         }
 
