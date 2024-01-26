62730bd929bbe936a396d5eb6198e4f093f724d5
Florian Katerndahl <florian@katerndahl.com>
2024-01-26 15:21:36 +0000

    Fri, Jan 26, 2024, 3:21 PM +00:00

---
+++ speller/hash-test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dictionary.h"
+
+// Undefine any definitions
+#undef calculate
+#undef getrusage
+
+// Default dictionary
+#define DICTIONARY "dictionaries/large"
+
+int main(void)
+{
+
+}
