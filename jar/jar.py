7b5e3e09e17105354ffc70e49b4260e89564f987
Florian Katerndahl <florian@katerndahl.com>
2024-01-27 21:03:07 +0000

    Sat, Jan 27, 2024, 9:03 PM +00:00

---
+++ jar/jar.py
@@ -0,0 +1,37 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError("Who has a jar that fits a negative amount of cookies?")
+        self.capacity = capacity
+        self.size = 0
+
+    def __str__(self):
+        return "🍪" * self.size
+
+    def deposit(self, n):
+        if self.size + n > self.capacity:
+            raise ValueError("Not enough place for additional cookies :(")
+        self.size += n
+
+    def withdraw(self, n):
+        if self.size - n < 0:
+            raise ValueError("Not enough cookies :(")
+        self.size -= n
+
+    @property
+    def capacity(self):
+        return self.capacity
+
+    @property
+    def size(self):
+        return self.size
+
+
+def main():
+    jar = Jar()
+    jar.deposit(100)
+    print(jar)
+    jar.withdraw(50)
+    print(jar.capacity, jar.size)
+
+main()
