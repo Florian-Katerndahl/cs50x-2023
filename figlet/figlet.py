b2163338487d6dc533684d916152310c4b810e6a
Florian Katerndahl <florian@katerndahl.com>
2024-01-27 20:48:53 +0000

    Sat, Jan 27, 2024, 8:48 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,20 @@
 import sys
-import pyfiglet
+from pyfiglet import Figlet
 
 if sys.argc != 1 or sys.argc != 3:
-    print("ERROR: Invalid usage")
-    print("figlet.py [-f|--font fontname]")
+    print("Invalid usage")
     sys.exit(1)
 
+f = Figlet()
+
 if sys.argc == 3:
-    
+    if sys.argv[1] not in ["-f", "--force"]:
+        print("Invalid usage")
+        sys.exit(1)
+    f = Figlet(font=sys.argv[2])
+
+user_input = input("Input: ")
+
+print("Output")
+
+print(f.renderText())
