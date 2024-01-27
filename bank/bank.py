4d3790bff6c06a4e191cb580906e987aa722458b
Florian Katerndahl <florian@katerndahl.com>
2024-01-27 20:35:37 +0000

    Sat, Jan 27, 2024, 8:35 PM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,7 +2,7 @@ greeting = input("Greeting: ")
 
 if greeting.split()[0].lower() == "hello":
     print("$0")
-elif greeting.split()[0].lower() == "h":
+elif greeting.split()[0].lower()[0] == "h":
     print("$20")
 else:
     print("$100")
