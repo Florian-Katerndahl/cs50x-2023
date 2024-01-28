37f28fac6cfcddaf2fe7e849fe856591947bad4b
Florian Katerndahl <florian@katerndahl.com>
2024-01-28 13:13:41 +0000

    Sun, Jan 28, 2024, 1:13 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,4 +5,4 @@ while height < 1 or height > 8:
     height = get_int("Height: ")
 
 for i in range(1, height + 1):
-    print(" " * (height - i) + "#" * i + "  " + "#" * i + " " * (height - i))
+    print(" " * (height - i) + "#" * i + "  " + "#" * i)
