a34a4c964715bf36e05f9190c41a7bc638289d53
Florian Katerndahl <florian@katerndahl.com>
2024-01-28 13:31:49 +0000

    Sun, Jan 28, 2024, 1:31 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -7,21 +7,21 @@ while change_owed < 0:
 
 change_owed = int(change_owed * 100)
 
-n_quarters = change_owed / 25
+n_quarters = change_owed // 25
 change_owed -= n_quarters * 25
 
-n_dimes = change_owed / 10
+n_dimes = change_owed // 10
 change_owed -= n_dimes * 1
 
-n_nickels = change_owed / 5
+n_nickels = change_owed // 5
 change_owed -= n_nickels * 5
 
-n_pennies = change_owed / 1
+n_pennies = change_owed // 1
 change_owed -= n_pennies * 1
 
-print(f"{n_quarters}")
+print(n_quarters + n_dimes + n_nickels + n_pennies)
 
 if change_owed:
     raise ValueError(f"The fuck... {change_owed = }")
 
-#print(f"{int(n_quarters + n_dimes + n_nickels + n_pennies)}")
+print(f"{int(n_quarters + n_dimes + n_nickels + n_pennies)}")
