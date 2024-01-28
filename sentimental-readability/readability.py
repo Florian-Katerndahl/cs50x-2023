e02e50efdc3ee2c8eb141ae241bff1034acfec7a
Florian Katerndahl <florian@katerndahl.com>
2024-01-28 15:55:32 +0000

    Sun, Jan 28, 2024, 3:55 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -12,7 +12,7 @@ def main():
 
 def coleman_liau(text):
     word_count = len(text.split())
-    sentence_count = len(text.split("."))
+    sentence_count = len(text.split("!?."))
 
     letter_count = 0
     for letter in text:
@@ -22,7 +22,7 @@ def coleman_liau(text):
     avg_letters = letter_count / word_count * 100.0
     avg_sentences = sentence_count / word_count * 100.0
 
-    print(avg_letters, avg_sentences)
+    print(word_count, sentence_count, letter_count, avg_letters, avg_sentences)
 
     return round(0.0588 * avg_letters - 0.296 * avg_sentences - 15.8)
 
