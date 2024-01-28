c6810294b2cb208dfa5cd30694887896b7357987
Florian Katerndahl <florian@katerndahl.com>
2024-01-28 12:37:11 +0000

    Sun, Jan 28, 2024, 12:37 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -18,9 +18,13 @@ def main():
     # TODO: Read teams into memory from file
     with open(sys.argv[1], "rt") as f:
         reader = csv.DictReader(f)
+        for row in reader:
+            row.update({"rating": int(row.get("rating"))})
+            teams.append(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    simulate_tournatement(teams)
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
