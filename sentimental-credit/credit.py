ffb40259cf81a78107ac4dfd4bada67654f19efc
Florian Katerndahl <florian@katerndahl.com>
2024-01-28 15:22:48 +0000

    Sun, Jan 28, 2024, 3:22 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,24 +1,50 @@
 from cs50 import get_string
 
 def main():
-    print(luhn("4003600000000014"))
+    card = get_string("Number: ")
+    if len(card) not in [13, 15, 16] or not luhn(card):
+        print("INVALID")
+    elif is_master(card):
+        print("MASTER")
+    elif is_visa(card):
+        print("VISA")
+    elif is_amex(card):
+        print("AMEX")
+    else:
+        print("UNREACHABLE")
 
 def luhn(card_number):
-    sum = 0
+    sum = []
     for i in range(len(card_number) - 1, -1, -2):
-        sum += int(card_number[i-1]) * 2
-    print(sum)
+        sum += [int(card_number[i-1]) * 2]
 
     sos = 0
-    for digit in str(sum):
-        sos += int(digit)
-    print(sos)
+    for s in sum:
+        for digit in str(s):
+            sos += int(digit)
 
     sum = 0
     for i in range(len(card_number), 0, -2):
         sum += int(card_number[i-1])
-    print(sum)
 
-    return (sum + sos) % 10
+    return (sum + sos) % 10 == 0
+
+
+def is_amex(card_number):
+    if len(card_number) == 15 and card_number[:2] in ["34", "37"]:
+        return True
+    return False
+
+
+def is_visa(card_number):
+    if len(card_number) in [13, 16] and card_number[0] == "4":
+        return True
+    return False
+
+
+def is_master(card_number):
+    if len(card_number) == 16 and card_number[:2] in ["51", "52", "53", "54", "55"]:
+        return True
+    return False
 
 main()
