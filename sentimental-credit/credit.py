1c2e8f689295356128b537e511cce73f0ccd71fd
Florian Katerndahl <florian@katerndahl.com>
2024-01-28 15:14:17 +0000

    Sun, Jan 28, 2024, 3:14 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,12 +1,24 @@
 from cs50 import get_string
 
 def main():
-    luhn("4003600000000014")
+    print(luhn("4003600000000014"))
 
 def luhn(card_number):
     sum = 0
     for i in range(len(card_number) - 1, -1, -2):
         sum += int(card_number[i-1]) * 2
-    
+    print(sum)
+
+    sos = 0
+    for digit in str(sum):
+        sos += int(digit)
+    print(sos)
+
+    sum = 0
+    for i in range(len(card_number), 0, -2):
+        sum += int(card_number[i-1])
+    print(sum)
+
+    return (sum + sos) % 10
 
 main()
