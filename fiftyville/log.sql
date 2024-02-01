9b47aea2df8d32d7067c15cf1a426c6c1ca1030f
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 12:21:30 +0000

    Thu, Feb 1, 2024, 12:21 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- get id from crime reports that mentions theft of CS50 duck: 295
+SELECT id FROM crime_scene_reports
+WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street" AND description LIKE "%duck%";
