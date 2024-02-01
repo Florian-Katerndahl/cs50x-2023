cde6a28f9597927ce98dbc02176d0ae5a7d93b9c
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 11:49:48 +0000

    Thu, Feb 1, 2024, 11:49 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,6 +1,6 @@
 SELECT * FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE people.name = "Bradley Cooper" OR people.name = "Jennifer Lawrence"
+--WHERE people.name = "Bradley Cooper" AND people.name = "Jennifer Lawrence"
 GROUP BY movies.id
 LIMIT 10;
