f9071b461f7c3f05c9b9e21a683673fea69be73b
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 09:32:04 +0000

    Thu, Feb 1, 2024, 9:32 AM +00:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -21,3 +21,33 @@ WHERE title LIKE "Community";
 UPDATE shows
 SET title = "Family Guy"
 WHERE title LIKE "Family%";
+
+UPDATE shows
+SET title = "Friends"
+WHERE title LIKE "Friends";
+
+UPDATE shows
+SET title = "Game of Thrones"
+WHERE title LIKE "game of thrones";
+
+UPDATE shows
+SET title = "Gilmore Girls"
+WHERE title LIKE "Gilmore Girls";
+
+UPDATE shows
+SET title = "Grey's Anatomy"
+WHERE title LIKE "Grey's Anatomy";
+
+UPDATE shows
+SET title = "How I Met Your Mother"
+WHERE title LIKE "How%";
+
+UPDATE shows
+SET title = "It's Always Sunny in Philadelphia"
+WHERE title LIKE "It's%";
+
+UPDATE shows
+SET title = "Parks and Recreation"
+WHERE title LIKE "Parks%";
+
+
