fb48593a212a57d05d0eb966bf30af79417b69e8
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 15:56:49 +0000

    Thu, Feb 1, 2024, 3:56 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,13 +1,60 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- get id from crime reports that mentions theft of CS50 duck: 295
+-- duck was stolen at 10:15am
 SELECT id, description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street" AND description LIKE "%duck%";
 
 -- get transcript of interviews with witnesses
 -- Ruth: thief drove away within 10 minutes
 -- Eugene: thief withdrew money at Legnett Street in the morning
--- Raymond: planning to take earliest flight out of fiftyville; ticket purchased by someone else
+-- Raymond: talked on the phone, planning to take earliest flight out of fiftyville on next day; ticket purchased by someone else
 SELECT name, transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 
+-- get cars leaving the bakery around time of theft (+/- 10 minutes)
+-- +---------------+
+-- | license_plate |
+-- +---------------+
+-- | 5P2BI95       |
+-- | 94KL13X       |
+-- | 6P58WS2       |
+-- | 4328GD8       |
+-- | G412CB7       |
+-- | L93JTIZ       |
+-- | 322W7JE       |
+-- | 0NTHK55       |
+-- +---------------+
+SELECT license_plate FROM bakery_security_logs
+WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 5 AND minute <= 25 AND activity = "exit";
+
+-- get people based on license plates
+-- +--------+---------+----------------+-----------------+---------------+
+-- |   id   |  name   |  phone_number  | passport_number | license_plate |
+-- +--------+---------+----------------+-----------------+---------------+
+-- | 221103 | Vanessa | (725) 555-4692 | 2963008352      | 5P2BI95       |
+-- | 243696 | Barry   | (301) 555-4174 | 7526138472      | 6P58WS2       |
+-- | 396669 | Iman    | (829) 555-5269 | 7049073643      | L93JTIZ       |
+-- | 398010 | Sofia   | (130) 555-0289 | 1695452385      | G412CB7       |
+-- | 467400 | Luca    | (389) 555-5198 | 8496433585      | 4328GD8       |
+-- | 514354 | Diana   | (770) 555-1861 | 3592750733      | 322W7JE       |
+-- | 560886 | Kelsey  | (499) 555-9472 | 8294398571      | 0NTHK55       |
+-- | 686048 | Bruce   | (367) 555-5533 | 5773159633      | 94KL13X       |
+-- +--------+---------+----------------+-----------------+---------------+
+SELECT * FROM people
+WHERE license_plate IN (
+    SELECT license_plate FROM bakery_security_logs
+    WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 5 AND minute <= 25 AND activity = "exit"
+);
+
+-- filter flights
+SELECT * FROM flights
+JOIN passengers ON flights.id = passengers.flight_id
+JOIN people ON passengers.passport_number = people.passport_number
+WHERE passengers.passport_number IN (
+    SELECT passport_number FROM people
+    WHERE license_plate IN (
+        SELECT license_plate FROM bakery_security_logs
+        WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 5 AND minute <= 25 AND activity = "exit"
+    )
+) AND flights.year = 2021 AND flights.month = 7 AND flights.day = 29;
