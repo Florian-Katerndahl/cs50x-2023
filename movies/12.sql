f07f4f9646d2b1b64c9bee7f33d243a8a5c1cc22
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 11:50:33 +0000

    Thu, Feb 1, 2024, 11:50 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
-SELECT * FROM movies
+SELECT title, COUNT(*) FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 --WHERE people.name = "Bradley Cooper" AND people.name = "Jennifer Lawrence"
