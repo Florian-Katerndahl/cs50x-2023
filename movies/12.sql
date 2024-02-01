26ede1476b8de5f20e0419cf7565060a696a0fbb
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 11:53:11 +0000

    Thu, Feb 1, 2024, 11:53 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,6 +1,6 @@
-SELECT title, COUNT(*) FROM movies
+SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
---WHERE people.name = "Bradley Cooper" AND people.name = "Jennifer Lawrence"
+WHERE stars.person_id IN (people.name = "Bradley Cooper", people.name = "Jennifer Lawrence")
 GROUP BY movies.id
 LIMIT 10;
