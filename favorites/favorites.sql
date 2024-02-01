48c03bebc86a63979dd8dfc44e3f460488b031ff
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 09:25:54 +0000

    Thu, Feb 1, 2024, 9:25 AM +00:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,2 +1,15 @@
 UPDATE shows
-SET title = "Adventure Time" 
+SET title = "Adventure Time"
+WHERE title LIKE "Adventure Time";
+
+UPDATE shows
+SET title = "Arrow"
+WHERE title LIKE "Arrow";
+
+UPDATE shows
+SET title = "Avatar: The Last Airbender"
+WHERE title LIKE "Avatar: The%";
+
+UPDATE shows
+SET title = "Brookly Nine-Nine"
+WHERE title LIKE 
