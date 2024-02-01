5c5f6b27f00696fb64e458862001ef401229eb73
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 12:27:41 +0000

    Thu, Feb 1, 2024, 12:27 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,12 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- get id from crime reports that mentions theft of CS50 duck: 295
-SELECT id FROM crime_scene_reports
+SELECT id, description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street" AND description LIKE "%duck%";
 
 -- get transcript of interviews with witnesses
--- thief drove away within 10 minutes
--- thief withdrew money at Legnett Street in the morning
--- 
-SELECT * FROM interviews
-WHERE transcript LIKE "%bakery%";
+-- Ruth: thief drove away within 10 minutes
+-- Eugene: thief withdrew money at Legnett Street in the morning
+-- Raymond: planning to take earliest flight out of fiftyville; ticket purchased by someone else
+SELECT name, transcript FROM interviews
+WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
+
