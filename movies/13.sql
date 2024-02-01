075c5e2bed64c581f3dcfb4c415703fe4f34527e
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 12:06:44 +0000

    Thu, Feb 1, 2024, 12:06 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,10 @@
 SELECT people.name FROM movies
 JOIN stars ON movies.id = stars.movie_id
---JOIN people ON stars.person_id = people.id
-WHERE people.name = "Kevin Bacon"
+JOIN people ON stars.person_id = people.id
+WHERE movies.id IN (
+    SELECT movies.id FROM movies
+    JOIN stars ON movies.id = stars.movie_id
+    JOIN people ON stars.person_id = people.id
+    WHERE people.name = "Kevin Bacon" AND birth = 1958
+) AND NOT (people.name = "Kevin Bacon" AND birth = 1958);
+
