2eb0814add36ceb02c98676d68625f5d0f34b8e1
Florian Katerndahl <florian@katerndahl.com>
2024-02-01 10:54:32 +0000

    Thu, Feb 1, 2024, 10:54 AM +00:00

--- prophecy/students.py
+++ prophecy/students.py
@@ -5,7 +5,6 @@ db = SQL("sqlite:///roster.db")
 
 rows = []
 houses = set()
-heads = set()
 with open("students.csv", "rt") as f:
     reader = csv.DictReader(f)
     for row in reader:
@@ -16,9 +15,13 @@ for row in rows:
                "VALUES (?)", row["student_name"])
     houses.add((row["house"], row["head"]))
 
-print(houses)
-
 for house in houses:
     db.execute("INSERT INTO houses (name, head)"
                "VALUES (?, ?)", house[0], house[1])
 
+students = db.excute("SELECT id, name FROM students")
+houses = db.execute("SELECT id, house FROM houses")
+
+for student in students:
+    db.execute("INSERT INTO house_assignments (student_id, house_id)"
+               "VALUES (?, ?)", )
