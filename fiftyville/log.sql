1465d7220a26505976495935bca792f8c00b824f
Florian Katerndahl <florian@katerndahl.com>
2024-02-02 16:08:00 +0000

    Fri, Feb 2, 2024, 4:08 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -12,35 +12,9 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street" AND 
 SELECT name, transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 
--- get cars leaving the bakery around time of theft (+/- 10 minutes)
--- +---------------+
--- | license_plate |
--- +---------------+
--- | 5P2BI95       |
--- | 94KL13X       |
--- | 6P58WS2       |
--- | 4328GD8       |
--- | G412CB7       |
--- | L93JTIZ       |
--- | 322W7JE       |
--- | 0NTHK55       |
--- +---------------+
 SELECT license_plate FROM bakery_security_logs
 WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 5 AND minute <= 25 AND activity = "exit";
 
--- get people based on license plates
--- +--------+---------+----------------+-----------------+---------------+
--- |   id   |  name   |  phone_number  | passport_number | license_plate |
--- +--------+---------+----------------+-----------------+---------------+
--- | 221103 | Vanessa | (725) 555-4692 | 2963008352      | 5P2BI95       |
--- | 243696 | Barry   | (301) 555-4174 | 7526138472      | 6P58WS2       |
--- | 396669 | Iman    | (829) 555-5269 | 7049073643      | L93JTIZ       |
--- | 398010 | Sofia   | (130) 555-0289 | 1695452385      | G412CB7       |
--- | 467400 | Luca    | (389) 555-5198 | 8496433585      | 4328GD8       |
--- | 514354 | Diana   | (770) 555-1861 | 3592750733      | 322W7JE       |
--- | 560886 | Kelsey  | (499) 555-9472 | 8294398571      | 0NTHK55       |
--- | 686048 | Bruce   | (367) 555-5533 | 5773159633      | 94KL13X       |
--- +--------+---------+----------------+-----------------+---------------+
 SELECT * FROM people
 WHERE license_plate IN (
     SELECT license_plate FROM bakery_security_logs
