47c69a88ac5c2a1b4924b53eb2ee064c50a0479e
Florian Katerndahl <florian@katerndahl.com>
2024-02-02 15:54:23 +0000

    Fri, Feb 2, 2024, 3:54 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,7 +7,7 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street" AND 
 
 -- get transcript of interviews with witnesses
 -- Ruth: thief drove away within 10 minutes
--- Eugene: thief withdrew money at Legnett Street in the morning
+-- Eugene: thief withdrew money at Leggett Street in the morning
 -- Raymond: talked on the phone, planning to take earliest flight out of fiftyville on next day; ticket purchased by someone else
 SELECT name, transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
@@ -47,8 +47,9 @@ WHERE license_plate IN (
     WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 5 AND minute <= 25 AND activity = "exit"
 );
 
--- filter flights
-SELECT * FROM flights
+-- filter flights taking of one day after theft with at least two pepole boarded from person table where one's license plate was recorderd
+-- flight ID 36 has thief and comapgnion
+SELECT flights.id FROM flights
 JOIN passengers ON flights.id = passengers.flight_id
 JOIN people ON passengers.passport_number = people.passport_number
 WHERE passengers.passport_number IN (
@@ -57,4 +58,45 @@ WHERE passengers.passport_number IN (
         SELECT license_plate FROM bakery_security_logs
         WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 5 AND minute <= 25 AND activity = "exit"
     )
-) AND flights.year = 2021 AND flights.month = 7 AND flights.day = 29;
+) AND flights.year = 2021 AND flights.month = 7 AND flights.day = 29
+GROUP BY flights.id
+HAVING COUNT(*) >= 2;
+
+-- flight destination of flight on next day
+SELECT airports.full_name FROM flights
+JOIN airports ON flights.destination_airport_id = airports.id
+WHERE flights.id = 36;
+
+-- thief withdrew money
+-- either thief or person who helped thief returned
+-- number in question: (389) 555-5198
+SELECT people.id, people.name, people.phone_number, flights.id FROM atm_transactions
+JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number
+JOIN people ON bank_accounts.person_id = people.id
+JOIN passengers ON people.passport_number = passengers.passport_number
+JOIN flights ON passengers.flight_id = flights.id
+JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+WHERE atm_transactions.year = 2021 AND atm_transactions.month = 7 AND atm_transactions.day = 28 AND atm_transactions.atm_location = "Leggett Street" AND atm_transactions.transaction_type = "withdraw"
+    AND (bakery_security_logs.year = 2021 AND bakery_security_logs.month = 7 AND bakery_security_logs.day = 28 AND bakery_security_logs.hour = 10 AND bakery_security_logs.minute >= 5 AND bakery_security_logs.minute <= 25 AND bakery_security_logs.activity = "exit")
+    AND (flights.year = 2021 AND flights.month = 7 AND flights.day = 29)
+GROUP BY flights.id
+HAVING COUNT(*) >= 2;
+
+-- suspected thief or helper talked on phone with whom?
+-- Luca was called by Walter and Kathryn, only ... was also on flight ID 36 to LaGuardia Airport
+SELECT * FROM phone_calls
+JOIN (SELECT phone_number as caller_num, name as pcaller FROM people) ON caller_num = phone_calls.caller
+JOIN (SELECT phone_number as reciever_num, name as pcallee FROM people) ON reciever_num = phone_calls.receiver
+WHERE year = 2021 AND month = 7 AND day = 28 AND (caller_num = "(389) 555-5198"OR reciever_num = "(389) 555-5198");
+
+
+SELECT people.name FROM flights
+JOIN passengers ON flights.id = passengers.flight_id
+JOIN people ON passengers.passport_number = people.passport_number
+WHERE passengers.passport_number IN (
+    SELECT passport_number FROM people
+    WHERE license_plate IN (
+        SELECT license_plate FROM bakery_security_logs
+        WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 5 AND minute <= 25 AND activity = "exit"
+    )
+) AND flights.year = 2021 AND flights.month = 7 AND flights.day = 29 AND flights.id = 36;
